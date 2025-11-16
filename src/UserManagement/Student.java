5cd5b3b7dce12717d9228f8cd71e2548f4a5ff0c
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-11-16 17:46:44 +0200

    added student functionality

--- src/UserManagement/Student.java
+++ src/UserManagement/Student.java
@@ -1,8 +1,49 @@
 package UserManagement;
 
+import CourseManagement.Course;
+import CourseManagement.Lesson;
+import Database.CourseService;
+import java.util.*;
+
 public class Student extends User{
     public Student(String name, String ID, String email, String password){
         super(name, ID, email, password);
         this.role = "Student";
+    }
+
+    public void enrollInCourse(CourseService courseService, String CourseID, Student student) {
+        Course course = courseService.getRecord(CourseID);
+        if (course != null) {
+            course.addStudent(student);
+        } else {
+            throw new IllegalArgumentException("Invalid Course ID");
+        }
+    }
+
+    public void dropCourse(CourseService courseService, String CourseID, Student student) {
+        Course course = courseService.getRecord(CourseID);
+        if (course != null) {
+            course.removeStudent(student.getSearchKey());
+        } else {
+            throw new IllegalArgumentException("Invalid Course ID");
+        }
+    }
+
+    public List<Lesson> getAllLessons(CourseService courseService, String CourseID) {
+        Course course = courseService.getRecord(CourseID);
+        if (course != null) {
+            return Collections.unmodifiableList(course.getLessons());
+        } else {
+            throw new IllegalArgumentException("Invalid Course ID");
+        }
+    }
+
+    public Lesson getLesson(CourseService courseService, String CourseID, String lessonID) {
+        Course course = courseService.getRecord(CourseID);
+        if (course != null) {
+            return course.searchLesson(lessonID);
+        } else {
+            throw new IllegalArgumentException("Invalid Course ID");
+        }
     }
 }
