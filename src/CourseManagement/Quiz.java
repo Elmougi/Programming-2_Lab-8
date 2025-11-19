f7b2777b7daee49676738b71673fa75f54ec5b0d
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-11-19 09:58:58 +0200

    made the question as a separate object

--- src/CourseManagement/Quiz.java
+++ src/CourseManagement/Quiz.java
@@ -5,21 +5,22 @@ import java.util.*;
 
 public class Quiz {
 //    private List<String[]> quizData;
-    private List<String> questions;
-    private List<List<String>> options;
-    private List<Integer> answersIndex;
+//    private List<String> questions;
+//    private List<List<String>> options;
+//    private List<Integer> answersIndex;
     // private Map<Map<String, List<String>>, Integer> quizData;
     // Map that contains question+options and the answer index in the options
     // the questions+options is a map where the options is a list of strings
-
+    private List<Question> questions;
 
     public Quiz() {
         this.questions = new ArrayList<>();
-        this.options = new ArrayList<>();
-        this.answersIndex = new  ArrayList<>();
     }
-    public Quiz(List<String> questions, List<List<String>> options, List<Integer> answerIndex) {
-        setQuizData(questions, options, answersIndex);
+    public Quiz(List<Question> questions) {
+        if(questions != null)
+            this.questions = questions;
+        else
+            this.questions = new ArrayList<>();
     }
 //    public Quiz(String quizId, Map<Map<String, List<String>>, Integer> quizData) {
 //        this.quizId = quizId;
@@ -27,56 +28,98 @@ public class Quiz {
 //        else this.quizData = new HashMap<>();
 //    }
 
-    public Map<String, List<String>> getQuizData() {
-        Map<String, List<String>> res = new HashMap<>();
+//    public Map<String, List<String>> getQuizData() {
+//        Map<String, List<String>> res = new HashMap<>();
+//
+//        for(int i=0; i < questions.size(); i++) {
+//            res.put(questions.get(i), options.get(i));
+//        }
+//
+//        return Collections.unmodifiableMap(res);
+//    }
+//
+//    public List<String> getQuestions() {
+//        return Collections.unmodifiableList(questions);
+//    }
+//
+//    public List<List<String>> getOptions() {
+//        return Collections.unmodifiableList(options);
+//    }
+//
+//    public List<Integer> getAnswersIndex() {
+//        return Collections.unmodifiableList(answersIndex);
+//    }
+//
+//    public void setQuizData(List<String> questions, List<List<String>> options, List<Integer> answersIndex) {
+//        if(questions.size() != options.size() || answersIndex.size() != questions.size()) {
+//            throw new IllegalArgumentException("Number of questions and answers must be the same.");
+//        }
+//
+//        this.questions = questions;
+//        this.options = options;
+//        this.answersIndex = answersIndex;
+//    }
+//
+//    public void addQuizQuestion(String question, List<String> options, int answerIndex) {
+//        if(answerIndex < 0 || answerIndex >= options.size()) {
+//            throw new IllegalArgumentException("Answer index is out of bounds.");
+//        }
+//
+//        this.questions.add(question);
+//        this.options.add(options);
+//        this.answersIndex.add(answerIndex);
+//    }
+//
+//    public boolean isCorrect(int index, int answerIndex) {
+//        if(index < 0 || index >= questions.size()) {
+//            throw new IllegalArgumentException("Question index is out of bounds.");
+//        }
+//
+//        return this.answersIndex.get(index) == answerIndex;
+//    }
+//
+//    public double calculateScore(List<Integer> userAnswers) {
+//        if(userAnswers.size() != questions.size()) {
+//            throw new IllegalArgumentException("Number of user answers must match number of questions.");
+//        }
+//
+//        int correctCount = 0;
+//        for(int i = 0; i < questions.size(); i++) {
+//            if(isCorrect(i, userAnswers.get(i))) {
+//                correctCount++;
+//            }
+//        }
+//
+//        return (double) correctCount / questions.size() * 100;
+//    }
 
-        for(int i=0; i < questions.size(); i++) {
-            res.put(questions.get(i), options.get(i));
-        }
 
-        return Collections.unmodifiableMap(res);
+    public List<Question> getQuestions() {
+        return questions;
     }
 
-    public List<String> getQuestions() {
-        return Collections.unmodifiableList(questions);
+    public void setQuestions(List<Question> questions) {
+        if(questions != null)
+            this.questions = questions;
     }
 
-    public void setQuizData(List<String> questions, List<List<String>> options, List<Integer> answersIndex) {
-        if(questions.size() != options.size() || answersIndex.size() != questions.size()) {
-            throw new IllegalArgumentException("Number of questions and answers must be the same.");
+    public void addQuizQuestion(Question question) {
+        if(question != null) {
+            this.questions.add(question);
         }
-
-        this.questions = questions;
-        this.options = options;
-        this.answersIndex = answersIndex;
-    }
-
-    public void addQuizQuestion(String question, List<String> options, int answerIndex) {
-        if(answerIndex < 0 || answerIndex >= options.size()) {
-            throw new IllegalArgumentException("Answer index is out of bounds.");
-        }
-
-        this.questions.add(question);
-        this.options.add(options);
-        this.answersIndex.add(answerIndex);
-    }
-
-    public boolean isCorrect(int index, int answerIndex) {
-        if(index < 0 || index >= questions.size()) {
-            throw new IllegalArgumentException("Question index is out of bounds.");
-        }
-
-        return this.answersIndex.get(index) == answerIndex;
     }
 
     public double calculateScore(List<Integer> userAnswers) {
         if(userAnswers.size() != questions.size()) {
             throw new IllegalArgumentException("Number of user answers must match number of questions.");
         }
+        if(questions.isEmpty()) {
+            return 100;
+        }
 
         int correctCount = 0;
         for(int i = 0; i < questions.size(); i++) {
-            if(isCorrect(i, userAnswers.get(i))) {
+            if(questions.get(i).isCorrect(userAnswers.get(i))) {
                 correctCount++;
             }
         }
